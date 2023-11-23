6eec30525aeae5ef1227b0d5f791966d2636a35d
davidcofe <davidcofe@unisabana.edu.co>
2023-11-22 20:34:26 -0500

    Correcciones a H2

--- src/test/java/co/edu/unisabana/siga/banco/BancoApplicationTests.java
+++ src/test/java/co/edu/unisabana/siga/banco/BancoApplicationTests.java
@@ -3,22 +3,14 @@ package co.edu.unisabana.siga.banco;
 import co.edu.unisabana.siga.banco.bd.Cliente;
 import co.edu.unisabana.siga.banco.bd.ClienteRepository;
 import co.edu.unisabana.siga.banco.controller.dto.ClienteDTO;
-import co.edu.unisabana.siga.banco.controller.dto.RespuestaDTO;
 import co.edu.unisabana.siga.banco.logica.ClienteLogica;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest @Profile("test")
+@SpringBootTest
 class BancoApplicationTests {
 
 	@Mock
